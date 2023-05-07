7774dd97a5b21b08aeae080cee6e61f699b23a15
Husni Mubarak <husnimubarak859@gmail.com>
2023-05-08 03:32:45 +0700

    setup

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,11 +1,11 @@
-import Navbar from '@/components/Navbar';
-import Sidebar from '@/components/Sidebar';
-import BasicInformation from '@/components/profile/basicInfomation/BasicInformation';
-import Education from '@/components/profile/Education';
-import Resume from '@/components/profile/Resume';
-import SalaryExpectation from '@/components/profile/salaryExpectation/SalaryExpectation';
-import Skills from '@/components/profile/Skills';
-import WorkExperience from '@/components/profile/WorkExperience';
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+import BasicInformation from "@/components/profile/basicInfomation/BasicInformation";
+import Education from "@/components/profile/Education";
+import Resume from "@/components/profile/Resume";
+import SalaryExpectation from "@/components/profile/salaryExpectation/SalaryExpectation";
+import Skills from "@/components/profile/Skills";
+import WorkExperience from "@/components/profile/WorkExperience";
 
 export default function profile() {
   return (
