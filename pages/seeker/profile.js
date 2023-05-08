9ef96efc24f5a926323f54fc0d942e6bfcab77d6
Husni Mubarak <husnimubarak859@gmail.com>
2023-05-08 14:43:49 +0700

    no change

--- pages/seeker/profile.js
+++ pages/seeker/profile.js
@@ -1,12 +1,12 @@
-import Layout from '@/components/layout/Dashboard';
-import SalaryExpectation from '@/components/profile/salaryExpectation/SalaryExpectation';
-import BasicInformation from '@/components/profile/basicInfomation/BasicInformation';
-import WorkExperience from '@/components/profile/WorkExperience';
-import Education from '@/components/profile/Education';
-import Resume from '@/components/profile/Resume';
-import Skills from '@/components/profile/Skills';
-import Summary from '@/components/profile/summary/Summary';
-import Achievement from '@/components/profile/achievement/Achievement';
+import Layout from "@/components/layout/Dashboard";
+import SalaryExpectation from "@/components/profile/salaryExpectation/SalaryExpectation";
+import BasicInformation from "@/components/profile/basicInfomation/BasicInformation";
+import WorkExperience from "@/components/profile/WorkExperience";
+import Education from "@/components/profile/Education";
+import Resume from "@/components/profile/Resume";
+import Skills from "@/components/profile/Skills";
+import Summary from "@/components/profile/summary/Summary";
+import Achievement from "@/components/profile/achievement/Achievement";
 
 const Profile = ({ profile }) => {
   return (
@@ -28,8 +28,8 @@ export default Profile;
 export const getServerSideProps = async (context) => {
   const { token } = context.req.cookies;
 
-  const result = await fetch('http://localhost:8000/users/profile', {
-    headers: { Authorization: 'Bearer ' + token },
+  const result = await fetch("http://localhost:8000/users/profile", {
+    headers: { Authorization: "Bearer " + token },
   });
   const data = await result.json();
 
