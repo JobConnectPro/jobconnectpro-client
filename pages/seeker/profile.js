05857394da46cab6909d5abf33e28f0aadc8155c
Husni Mubarak <husnimubarak859@gmail.com>
2023-05-09 19:30:59 +0700

    edit profile workExperience

--- pages/seeker/profile.js
+++ pages/seeker/profile.js
@@ -1,14 +1,14 @@
-import Layout from '@/components/layout/Dashboard';
-import SalaryExpectation from '@/components/profile/salaryExpectation/SalaryExpectation';
-import BasicInformation from '@/components/profile/basicInfomation/BasicInformation';
-import WorkExperience from '@/components/profile/WorkExperience';
-import Education from '@/components/profile/education/Education';
-import Resume from '@/components/profile/Resume';
-import Summary from '@/components/profile/summary/Summary';
-import Achievement from '@/components/profile/achievement/Achievement';
-import Project from '@/components/profile/project/Project';
-import UserSkill from '@/components/profile/skill/Skill';
-import Organization from '@/components/profile/organization/Organization';
+import Layout from "@/components/layout/Dashboard";
+import SalaryExpectation from "@/components/profile/salaryExpectation/SalaryExpectation";
+import BasicInformation from "@/components/profile/basicInfomation/BasicInformation";
+// import WorkExperience from "@/components/profile/WorkExperience";
+import Education from "@/components/profile/education/Education";
+import Resume from "@/components/profile/Resume";
+import Summary from "@/components/profile/summary/Summary";
+import Achievement from "@/components/profile/achievement/Achievement";
+import Project from "@/components/profile/project/Project";
+import UserSkill from "@/components/profile/skill/Skill";
+import Organization from "@/components/profile/organization/Organization";
 
 const Profile = ({ profile }) => {
   console.log(profile);
@@ -20,8 +20,8 @@ const Profile = ({ profile }) => {
       <Achievement userProfile={profile} />
       <Project userProfile={profile} />
       <UserSkill userProfile={profile} />
-      <WorkExperience />
-      <Education />
+      {/* <WorkExperience /> */}
+      {/* <Education /> */}
       <Resume />
     </Layout>
   );
@@ -32,8 +32,8 @@ export default Profile;
 export const getServerSideProps = async (context) => {
   const { token } = context.req.cookies;
 
-  const result = await fetch('http://localhost:8000/users/profile', {
-    headers: { Authorization: 'Bearer ' + token },
+  const result = await fetch("http://localhost:8000/users/profile", {
+    headers: { Authorization: "Bearer " + token },
   });
   const data = await result.json();
 
