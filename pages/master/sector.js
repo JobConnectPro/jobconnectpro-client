9ea6eea7d0148ae019c261745edcd18d96daf40a
Husni Mubarak <husnimubarak859@gmail.com>
2023-05-12 12:36:18 +0700

    skills CRUD <login admin>

---
+++ pages/master/sector.js
@@ -0,0 +1,12 @@
+import Layout from "@/components/layout/Dashboard";
+import SectorsForm from "@/components/profile/sectors/SectorsForm";
+
+const Profile = ({ profile }) => {
+  return (
+    <Layout>
+      <SectorsForm />
+    </Layout>
+  );
+};
+
+export default Profile;
