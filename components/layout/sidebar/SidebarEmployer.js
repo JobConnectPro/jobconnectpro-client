6bea2add485c27df9ff9a6557005e9340fd5806f
Aruf Rachman Hakim <arufhakim@gmail.com>
2023-05-13 16:00:42 +0700

    fixing the code

--- components/layout/sidebar/SidebarEmployer.js
+++ components/layout/sidebar/SidebarEmployer.js
@@ -1,10 +1,15 @@
 import Link from 'next/link';
 import { CgProfile } from 'react-icons/cg';
 import { AiOutlineSearch } from 'react-icons/ai';
+import { RiBookmarkFill, RiTeamFill, RiBuilding4Fill } from 'react-icons/ri';
 
 const SidebarEmployer = () => {
   return (
     <>
+      <Link href={'/employer/companies'} className="flex gap-2 items-center cursor-pointer hover:text-blue-700">
+        <RiBuilding4Fill size={20} />
+        My Company
+      </Link>
       <Link href={'/employer/job'} className="flex gap-2 items-center cursor-pointer hover:text-blue-700">
         <AiOutlineSearch size={20} />
         My Job
