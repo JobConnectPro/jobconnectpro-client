68d120bc7a82d02484743a535d2298222f1faec7
Aruf Rachman Hakim <arufhakim@gmail.com>
2023-05-14 22:30:27 +0700

    MAS RIPA forgot password

---
+++ pages/forgot-password/index.js
@@ -0,0 +1,15 @@
+import Header from '@/components/Header';
+import Footer from '@/components/homepage/Footer';
+import ForgotPasswordForm from '@/components/auth/ForgotPassword';
+
+const UserForgotPassword = () => {
+  return (
+    <>
+      <Header />
+      <ForgotPasswordForm />
+      <Footer />
+    </>
+  );
+};
+
+export default UserForgotPassword;
