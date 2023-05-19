c4ebd34caa1fb3f7b9cfa0495340dd161f2d9f27
arthagp <arthagusfi11@gmail.com>
2023-05-19 22:12:18 +0700

    mengedit bagian pencarian

--- components/employer/company/Company.js
+++ components/employer/company/Company.js
@@ -4,11 +4,12 @@ import { getCompanies } from '@/modules/fetchCompanies';
 import CustomPagination from './CustomPagination';
 
 const Company = () => {
-  const [companies, setCompanies] = useState();
+  const [companies, setCompanies] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [totalItems, setTotalItems] = useState(0);
   const [searchQuery, setSearchQuery] = useState('');
+  const [searchInput, setSearchInput] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,16 +25,18 @@ const Company = () => {
     fetchData();
   }, [currentPage, searchQuery]);
 
-  const handleSearchQueryChange = (event) => {
-    setSearchQuery(event.target.value);
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
   };
 
-  const handleSearch = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchQuery(searchInput);
     setCurrentPage(1);
   };
 
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
+  const handleSearchInputChange = (event) => {
+    setSearchInput(event.target.value);
   };
 
   return (
@@ -41,19 +44,27 @@ const Company = () => {
       <div className="mb-5 h-screen">
         <div className="w-full bg-blue-700 sticky top-16">
           <div className="mx-6">
-            <div className="grid grid-cols-1 py-4 gap-2">
-              <form>
-                <div className="relative">
-                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
-                  <input
-                    type="search"
-                    value={searchQuery}
-                    onChange={handleSearchQueryChange}
-                    id="default-search"
-                    className="block w-full px-4 py-2 text-gray-900 rounded-lg "
-                    placeholder="Search company by name.."
-                    required
-                  />
+            <div className="py-4 gap-2 border-b border-slate-200">
+              <form onSubmit={handleSubmit} className="flex">
+                <div className="flex">
+                  <div className="relative flex-grow">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
+                    <input
+                      type="search"
+                      value={searchInput}
+                      id="default-search"
+                      name="search"
+                      className="block w-full px-4 py-2 text-gray-900 rounded-lg"
+                      placeholder="Search jobs by title.."
+                      onChange={handleSearchInputChange}
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="px-4 py-2 text-white bg-blue-500 rounded-lg ml-3 hover:bg-green-400"
+                  >
+                    Search
+                  </button>
                 </div>
               </form>
             </div>
@@ -61,12 +72,22 @@ const Company = () => {
         </div>
         <h1 className="text-3xl font-bold mx-6 my-4">Company</h1>
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mx-6">
-          {companies && companies.map((company) => <CompanyCard key={company.id} company={company}></CompanyCard>)}
-          {companies == null && <p>No companies found</p>}
+          {companies.length > 0 ? (
+            companies.map((company) => (
+              <CompanyCard key={company.id} company={company}></CompanyCard>
+            ))
+          ) : (
+            <p>No companies found</p>
+          )}
         </div>
         <div className="mt-8 mx-6">
-          <CustomPagination currentPage={currentPage} totalPages={totalPages} totalItems={totalItems} onPageChange={handlePageChange} />
-        </div>{' '}
+          <CustomPagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            totalItems={totalItems}
+            onPageChange={handlePageChange}
+          />
+        </div>
       </div>
     </>
   );
