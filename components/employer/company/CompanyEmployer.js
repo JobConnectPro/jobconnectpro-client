678b96ce2a0183bde020803e3cc054c4c0143841
Aruf Rachman Hakim <53162307+arufhakim@users.noreply.github.com>
2023-05-19 23:57:57 +0700

    fix search bar style (#72)

--- components/employer/company/CompanyEmployer.js
+++ components/employer/company/CompanyEmployer.js
@@ -6,6 +6,7 @@ import Link from 'next/link';
 const CompanyEmployer = () => {
   const [companies, setCompanies] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [searchInput, setSearchInput] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,8 +20,13 @@ const CompanyEmployer = () => {
     fetchData();
   }, [searchQuery]);
 
-  const handleSearchQueryChange = (event) => {
-    setSearchQuery(event.target.value);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchQuery(searchInput);
+  };
+
+  const handleSearchInputChange = (event) => {
+    setSearchInput(event.target.value);
   };
 
   return (
@@ -28,29 +34,32 @@ const CompanyEmployer = () => {
       <div className="mb-5 h-screen">
         <div className="w-full bg-blue-700 sticky top-16">
           <div className="mx-6">
-            <div className="grid grid-cols-1 lg:grid-cols-5 py-4 gap-2">
-              <form className="col-span-1 lg:col-span-4">
-                <div className="relative">
+            <div className="py-4 gap-2">
+              <form onSubmit={handleSubmit} className="flex">
+                <div className="relative flex-grow">
                   <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
                   <input
                     type="search"
-                    value={searchQuery}
-                    onChange={handleSearchQueryChange}
+                    value={searchInput}
                     id="default-search"
-                    className="block w-full px-4 py-2 text-gray-900 rounded-lg "
-                    placeholder="Search company by name.."
-                    required
+                    name="search"
+                    className="block w-full px-4 py-2 text-gray-900 rounded-lg"
+                    placeholder="Search company by title..."
+                    onChange={handleSearchInputChange}
                   />
                 </div>
+                <button type="submit" className="px-4 py-2 text-white bg-blue-500 rounded-lg ml-3 hover:bg-green-400">
+                  Search
+                </button>
               </form>
-              <Link href="/employer/companies/create" className="justify-self-end">
-                <button className="bg-blue-500 hover:bg-blue-900 text-white font-bold py-2 px-6 rounded">Add Company</button>
-              </Link>
             </div>
           </div>
         </div>
         <div className="grid grid-cols-2 my-4">
-          <h1 className="mx-6 text-3xl font-bold">Company</h1>
+          <h1 className="mx-6 text-3xl font-bold">My Company</h1>
+          <Link href="/employer/companies/create" className="justify-self-end">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded mx-6">Add Company</button>
+          </Link>
         </div>
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mx-6">
           {companies && companies.map((company) => <CompanyCard key={company.id} company={company}></CompanyCard>)}
--- components/seeker/job/JobList.js
+++ components/seeker/job/JobList.js
@@ -16,17 +16,10 @@ const JobList = () => {
 
   const fetchDataJobs = async () => {
     try {
-      const res = await getJobsList(
-        searchQuery,
-        currentPage,
-        perPage,
-        locationFilter,
-        typeFilter,
-        experienceFilter
-      );
+      const res = await getJobsList(searchQuery, currentPage, perPage, locationFilter, typeFilter, experienceFilter);
       setJobs(res.data);
       setTotalPages(res.totalPages);
-      console.log(res)
+      console.log(res);
     } catch (error) {
       console.error(error);
       // Handle error
@@ -35,14 +28,7 @@ const JobList = () => {
 
   useEffect(() => {
     fetchDataJobs();
-  }, [
-    searchQuery,
-    currentPage,
-    perPage,
-    locationFilter,
-    typeFilter,
-    experienceFilter,
-  ]);
+  }, [searchQuery, currentPage, perPage, locationFilter, typeFilter, experienceFilter]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -72,20 +58,12 @@ const JobList = () => {
     const pages = [];
     for (let i = 1; i <= totalPages; i++) {
       pages.push(
-        <button
-          key={i}
-          className={`border px-4 py-2 rounded ${
-            i === currentPage
-              ? 'bg-blue-500 text-white'
-              : 'bg-white text-gray-700'
-          }`}
-          onClick={() => handlePageChange(i)}
-        >
+        <button key={i} className={`border px-4 py-2 rounded ${i === currentPage ? 'bg-blue-500 text-white' : 'bg-white text-gray-700'}`} onClick={() => handlePageChange(i)}>
           {i}
         </button>
       );
     }
-    return <div className='flex items-center justify-center mt-4'>{pages}</div>;
+    return <div className="flex items-center justify-center mt-4">{pages}</div>;
   };
 
   const handleLocationChange = (e) => {
@@ -120,65 +98,43 @@ const JobList = () => {
     return true;
   });
 
-  console.log(experienceFilter)
+  console.log(experienceFilter);
 
   return (
-    <div className='mb-5'>
-      <div className='w-full bg-blue-700 sticky top-16'>
-        <div className='mx-6'>
-          <div className='py-4 gap-2 border-b border-slate-200'>
-            <form onSubmit={handleSubmit} className='flex'>
-              <div className='relative flex-grow'>
-                <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'></div>
-                <input
-                  type='search'
-                  defaultValue={searchQuery}
-                  id='default-search'
-                  name='search'
-                  className='block w-full px-4 py-2 text-gray-900 rounded-lg'
-                  placeholder='Search jobs by title..'
-                />
+    <div className="mb-5">
+      <div className="w-full bg-blue-700 sticky top-16">
+        <div className="mx-6">
+          <div className="py-4 gap-2 border-b border-slate-200">
+            <form onSubmit={handleSubmit} className="flex">
+              <div className="relative flex-grow">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
+                <input type="search" defaultValue={searchQuery} id="default-search" name="search" className="block w-full px-4 py-2 text-gray-900 rounded-lg" placeholder="Search jobs by title..." />
               </div>
-              <button
-                type='submit'
-                className='px-4 py-2 text-white bg-blue-500 rounded-lg ml-3 hover:bg-green-400'
-              >
+              <button type="submit" className="px-4 py-2 text-white bg-blue-500 rounded-lg ml-3 hover:bg-green-400">
                 Search
               </button>
             </form>
           </div>
         </div>
-        <div className='grid gap-x-5 mx-6 py-4 grid-cols-3 text-center items-center'>
-          <select
-            className='border p-2 rounded-lg text-gray-500'
-            value={locationFilter}
-            onChange={handleLocationChange}
-          >
-            <option value=''>Location (All)</option>
+        <div className="grid gap-x-5 mx-6 py-4 grid-cols-3 text-center items-center">
+          <select className="border p-2 rounded-lg text-gray-500" value={locationFilter} onChange={handleLocationChange}>
+            <option value="">Location (All)</option>
             {locations.map((location) => (
               <option key={location} value={location}>
                 {location}
               </option>
             ))}
           </select>
-          <select
-            className='border p-2 rounded-lg text-gray-500'
-            value={typeFilter}
-            onChange={handleTypeChange}
-          >
-            <option value=''>Type (All)</option>
+          <select className="border p-2 rounded-lg text-gray-500" value={typeFilter} onChange={handleTypeChange}>
+            <option value="">Type (All)</option>
             {types.map((type) => (
               <option key={type} value={type}>
                 {type}
               </option>
             ))}
           </select>
-          <select
-            className='border p-2 rounded-lg text-gray-500'
-            value={experienceFilter}
-            onChange={handleExperienceChange}
-          >
-            <option value=''>Experience (All)</option>
+          <select className="border p-2 rounded-lg text-gray-500" value={experienceFilter} onChange={handleExperienceChange}>
+            <option value="">Experience (All)</option>
             {experiences.map((experience) => (
               <option key={experience} value={experience}>
                 {experience}
@@ -187,26 +143,22 @@ const JobList = () => {
           </select>
         </div>
       </div>
-      <div className='flex justify-between my-4 mx-6'>
-        <div className='flex items-center mr-2'>
+      <div className="flex justify-between my-4 mx-6">
+        <div className="flex items-center mr-2">
           <span>Show:</span>
-          <select
-            className='mx-2 border rounded-lg p-1'
-            value={perPage}
-            onChange={handlePerPageChange}
-          >
-            <option value='10'>10</option>
-            <option value='20'>20</option>
-            <option value='50'>50</option>
+          <select className="mx-2 border rounded-lg p-1" value={perPage} onChange={handlePerPageChange}>
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="50">50</option>
           </select>
         </div>
       </div>
-      <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mx-6'>
-      {filteredJobs.map((job) => (
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mx-6">
+        {filteredJobs.map((job) => (
           <JobCard job={job} key={job.id} />
         ))}
       </div>
-      <div className='flex justify-center mt-4'>{renderPagination()}</div>
+      <div className="flex justify-center mt-4">{renderPagination()}</div>
     </div>
   );
 };
