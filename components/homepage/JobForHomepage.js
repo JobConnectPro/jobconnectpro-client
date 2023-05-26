47225b2b2971191a1a62c1c9043b0b65e6650b92
Aruf Rachman Hakim <arufhakim@gmail.com>
2023-05-26 14:58:05 +0700

    fix layout job post on homepage

--- components/homepage/JobForHomepage.js
+++ components/homepage/JobForHomepage.js
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import JobCardHome from "./JobCardHome";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import JobCardHome from './JobCardHome';
 
 export default function JobForHomepage() {
   const [jobs, setJobs] = useState([]);
@@ -10,10 +10,10 @@ export default function JobForHomepage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get("http://localhost:8000/job");
+        const res = await axios.get('http://localhost:8000/job');
         setJobs(res.data.data);
       } catch (error) {
-        console.error("Error fetching jobs:", error);
+        console.error('Error fetching jobs:', error);
       }
     };
 
@@ -32,10 +32,14 @@ export default function JobForHomepage() {
   const visibleJobs = jobs.slice(startIndex, startIndex + jobsPerSlide * 2);
 
   return (
-    <div className="w-full px-14 mx-auto">
-      <div className="grid grid-cols-2 gap-2">
-        <div className="col-span-2">
-          <div className="flex gap-4">
+    <div className="w-full px-14 items-center bg-white">
+      <h1 className="w-full text-center items-center mx-auto font-bold text-2xl md:text-3xl mb-8">
+        Find a Career <br />
+        <span className="text-blue-700"> That Works For You </span>
+      </h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+        <div className="col-span-1 lg:col-span-2">
+          <div className="flex flex-col lg:flex-row gap-4">
             {visibleJobs.slice(0, jobsPerSlide).map((job) => (
               <div key={job.id} className="flex-1">
                 <JobCardHome job={job} />
@@ -43,8 +47,8 @@ export default function JobForHomepage() {
             ))}
           </div>
         </div>
-        <div className="col-span-2 mt-4">
-          <div className="flex gap-4">
+        <div className="col-span-1 lg:col-span-2 mt-4">
+          <div className="flex flex-col lg:flex-row gap-4">
             {visibleJobs.slice(jobsPerSlide, jobsPerSlide * 2).map((job) => (
               <div key={job.id} className="flex-1">
                 <JobCardHome job={job} />
@@ -53,20 +57,12 @@ export default function JobForHomepage() {
           </div>
         </div>
       </div>
-      <div className="flex justify-center mt-4">
-        <button
-          className={`${
-            currentSlide === 0 ? "hidden" : "block"
-          } bg-white text-gray-500 px-4 py-2 rounded-md shadow-md focus:outline-none mr-2`}
-          onClick={handlePrevSlide}
-        >
+      <div className="flex justify-end mt-8">
+        <button className={`${currentSlide === 0 ? 'hidden' : 'block'} bg-blue-500 text-white px-4 py-2 rounded-md focus:outline-none`} onClick={handlePrevSlide}>
           Previous
         </button>
         {startIndex + jobsPerSlide * 2 < jobs.length && (
-          <button
-            className="bg-white text-gray-500 px-4 py-2 rounded-md shadow-md focus:outline-none ml-2"
-            onClick={handleNextSlide}
-          >
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-md focus:outline-none ml-2" onClick={handleNextSlide}>
             Next
           </button>
         )}
